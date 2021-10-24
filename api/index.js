f396b199e8f2b6a45192caaf71990ed222e1d0d7
Bruno Sentinelli <brunosentinelli@gmail.com>
2021-10-24 19:13:04 -0300

    back

--- api/index.js
+++ api/index.js
@@ -19,10 +19,12 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
+const {LoadDb} = require('./src/loadDb/loadDb')
 
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
   server.listen(3001, () => {
+    LoadDb();
     console.log('%s listening at 3001'); // eslint-disable-line no-console
   });
 });
